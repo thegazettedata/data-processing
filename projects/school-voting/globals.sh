ea17e41a36566b934a1ba15828f9c4c91e69512d
Chris Essig <csessig86@gmail.com>
2015-08-26 14:45:50 -0500

    Uncomment out process

--- projects/school-voting/globals.sh
+++ projects/school-voting/globals.sh
@@ -4,4 +4,17 @@ PROJECT_NAME="school-voting"
 EMPLOYEES=("ic" "cr")
 TOPICS=("joco" "linn")
 
-INFO="Analysis to see how many school employees voted in the last school board election"
+INFO="
+To reach these conclusions, The Gazette ran a sophisticated analysis involving four datasets that totaled roughly 250,000 rows in all.
+
+The spreadsheets we received were: every registered voter in Linn and Johnson county; and the salaries for every school employee in Iowa City and Cedar Rapids.
+
+We first merged the Linn and Johnson county voter information together into one spreadsheet. We then matched the names in the salary spreadsheet with all the voter names to see if school employees voted in the last election.
+
+For the Iowa City data, we also received addresses, so we were able to compare not only names but addresses between the voter and salary datasets. This extra criteria likely made the results we found in Iowa City more accurate.
+
+The technique we used to match names is called deduping, and the program we used is called csvdedupe, which was created by DataMade, a civic technology company based in Chicago, Illinois.
+
+The program used input by us to help match the names. All the code that ran this analysis is available on our Github page: 
+"
+
--- projects/school-voting/process.sh
+++ projects/school-voting/process.sh
@@ -8,10 +8,10 @@ source globals.sh
 # Query the DB
 function queryDB() {
 	echo "Show all names that dedupe matched on"
-	# cat sql/matching-$EMPLOYEE.sql | sqlite3 -header -csv $DB_EMPLOYEE_TWO > output/matching-$EMPLOYEE.csv
+	cat sql/matching-$EMPLOYEE.sql | sqlite3 -header -csv $DB_EMPLOYEE_TWO > output/matching-$EMPLOYEE.csv
 
 	echo "Show all names that dedupe didn't match on"
-	# cat sql/not-matching-$EMPLOYEE.sql | sqlite3 -header -csv $DB_EMPLOYEE_TWO > output/not-matching-$EMPLOYEE.csv
+	cat sql/not-matching-$EMPLOYEE.sql | sqlite3 -header -csv $DB_EMPLOYEE_TWO > output/not-matching-$EMPLOYEE.csv
 
 	echo "Find percent of employees who voted"
 	cat sql/percent-$EMPLOYEE.sql | sqlite3 -header -csv $DB_EMPLOYEE_TWO > output/percent-$EMPLOYEE.csv
@@ -32,12 +32,12 @@ function createDB() {
 	echo ".import $CSV_FIVE data" | sqlite3 -csv $DB_EMPLOYEE_TWO
 
 	# With DB created, we'll now query it
-	# queryDB
+	queryDB
 }
 
 function dedupeCSVs() {
 	echo "Stack Joco and Linn voting data"
-	# csvstack ${CSV_THREES[@]} > "$CSV_FOUR"
+	csvstack ${CSV_THREES[@]} > "$CSV_FOUR"
 
 	echo "Dedupe the voters and $EMPLOYEE employees"
 	csvlink $CSV_FOUR edits/employees/02-$EMPLOYEE-employees-uniq.csv \
@@ -50,23 +50,23 @@ function trimCSVs() {
 	echo "Remove unnecessary columns"
 	echo "$CSV_ONE > $CSV_TWO"
 
-	# if [ $topic == "joco" ]
-	# then
-	# 	csvcut $CSV_ONE -c REGN_NUM,FIRST_NAME,MIDDLE_NAME,LAST_NAME,PHONENO,HOUSE_NUM,HOUSE_SUFFIX,PRE_DIR,STREET_NAME,STREET_TYPE,POST_DIR,UNIT_TYPE,UNIT_NUM,CITY,STATE,ZIP_CODE,VOTERSTATUS,PARTY,GENDER,BIRTHDATE,SCHOOL_ELECTION_DATE_01,SCHOOL_VOTERVOTEMETHOD_01,SCHOOL_ELECTION_DATE_02,SCHOOL_VOTERVOTEMETHOD_02,SCHOOL_ELECTION_DATE_03,SCHOOL_VOTERVOTEMETHOD_03,SCHOOL_ELECTION_DATE_04,SCHOOL_VOTERVOTEMETHOD_04,SCHOOL_ELECTION_DATE_05,SCHOOL_VOTERVOTEMETHOD_05 > $CSV_TWO
-	# else
-	# 	csvcut $CSV_ONE -c REGN_NUM,FIRST_NAME,MIDDLE_NAME,LAST_NAME,PHONENO,HOUSE_NUM,HOUSE_SUFFIX,PRE_DIR,STREET_NAME,STREET_TYPE,POST_DIR,UNIT_TYPE,UNIT_NUM,CITY,STATE,ZIP_CODE,VOTERSTATUS,PARTY,GENDER,BIRTHDATE,SCHOOL_ELECTION_091013 > $CSV_TWO
-	# fi
+	if [ $topic == "joco" ]
+	then
+		csvcut $CSV_ONE -c REGN_NUM,FIRST_NAME,MIDDLE_NAME,LAST_NAME,PHONENO,HOUSE_NUM,HOUSE_SUFFIX,PRE_DIR,STREET_NAME,STREET_TYPE,POST_DIR,UNIT_TYPE,UNIT_NUM,CITY,STATE,ZIP_CODE,VOTERSTATUS,PARTY,GENDER,BIRTHDATE,SCHOOL_ELECTION_DATE_01,SCHOOL_VOTERVOTEMETHOD_01,SCHOOL_ELECTION_DATE_02,SCHOOL_VOTERVOTEMETHOD_02,SCHOOL_ELECTION_DATE_03,SCHOOL_VOTERVOTEMETHOD_03,SCHOOL_ELECTION_DATE_04,SCHOOL_VOTERVOTEMETHOD_04,SCHOOL_ELECTION_DATE_05,SCHOOL_VOTERVOTEMETHOD_05 > $CSV_TWO
+	else
+		csvcut $CSV_ONE -c REGN_NUM,FIRST_NAME,MIDDLE_NAME,LAST_NAME,PHONENO,HOUSE_NUM,HOUSE_SUFFIX,PRE_DIR,STREET_NAME,STREET_TYPE,POST_DIR,UNIT_TYPE,UNIT_NUM,CITY,STATE,ZIP_CODE,VOTERSTATUS,PARTY,GENDER,BIRTHDATE,SCHOOL_ELECTION_091013 > $CSV_TWO
+	fi
 
 	echo "Create database so we can concat"
 	DB_TOPIC="db/00-$topic.db"
 
-	# csvsql -i sqlite $CSV_TWO > sql/data-create-$topic.sql
-	# cat sql/data-create-$topic.sql | sqlite3 $DB_TOPIC
-	# echo ".import $CSV_TWO data" | sqlite3 -csv $DB_TOPIC
+	csvsql -i sqlite $CSV_TWO > sql/data-create-$topic.sql
+	cat sql/data-create-$topic.sql | sqlite3 $DB_TOPIC
+	echo ".import $CSV_TWO data" | sqlite3 -csv $DB_TOPIC
 
 	echo "Concat addresses and standarize voting records"
 	echo "$CSV_TWO > $CSV_THREE"
-	# cat sql/$topic-concat-addresses.sql | sqlite3 -header -csv $DB_TOPIC > $CSV_THREE
+	cat sql/$topic-concat-addresses.sql | sqlite3 -header -csv $DB_TOPIC > $CSV_THREE
 }
 
 
@@ -114,7 +114,7 @@ do
 	fi
 
 	# Trim up the CSVs and concat
-	# trimCSVs
+	trimCSVs
 done
 
 # Convert and trim employee data
@@ -141,26 +141,26 @@ do
 	echo "Create database so we can query data"
 	DB_EMPLOYEE="db/00-$employee-employees.db"
 
-	# csvsql -i sqlite $CSV_EMPLOYEES > sql/data-create-$employee.sql
-	# cat sql/data-create-$employee.sql | sqlite3 $DB_EMPLOYEE
-	# echo ".import $CSV_EMPLOYEES data" | sqlite3 -csv $DB_EMPLOYEE
+	csvsql -i sqlite $CSV_EMPLOYEES > sql/data-create-$employee.sql
+	cat sql/data-create-$employee.sql | sqlite3 $DB_EMPLOYEE
+	echo ".import $CSV_EMPLOYEES data" | sqlite3 -csv $DB_EMPLOYEE
 
 	echo "Query unique employees"
-	# cat sql/$employee-employees-uniq.sql | sqlite3 -header -csv $DB_EMPLOYEE > $CSV_EMPLOYEES_TWO
+	cat sql/$employee-employees-uniq.sql | sqlite3 -header -csv $DB_EMPLOYEE > $CSV_EMPLOYEES_TWO
 
 	# Dedupe the datasets
 	CSV_FOUR="edits/04-joco-linn-voters.csv"
 	CSV_FIVE="edits/05-$employee-employees-dedupe.csv"
 
 	# Dedupe voters and salary data
-	# dedupeCSVs
+	dedupeCSVs
 
 	# Create final DB so we can filter out those who voted
 	# And those who didn't
 	DB_EMPLOYEE_TWO="db/01-$employee-employees-dedupe.db"
 
 	# Run the DB tasks
-	# createDB
+	createDB
 
 	# Run the DB queries
 	queryDB
