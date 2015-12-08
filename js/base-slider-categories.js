4f4da858b02af00f7b95707ac303781147aa07bc
Chris Essig <csessig86@gmail.com>
2015-12-08 09:45:47 -0600

    Update map

--- js/base-slider-categories.js
+++ js/base-slider-categories.js
@@ -26,166 +26,28 @@
 // County and state averages for each category
 
 var slider_categories = {
-  "INDUSTRY": {
-    "text": "Industry",
+  "POPULATION": {
+    "text": "Population",
     "text-before": "",
-    "text-after": "%",
-    "categories": {
-      "subcategories": true,
-      "ARTS": {
-        "text": "Arts",
-        "county-avg": 0,
-        "min": 1,
-        "max": 12,
-        "state-avg": 7.5
-      },
-      "EDUCATION": {
-        "text": "Education",
-        "county-avg": 0,
-        "min": 17,
-        "max": 41,
-        "state-avg": 24.0
-      },
-      "FINANCE": {
-        "text": "Finance",
-        "county-avg": 0,
-        "min": 2,
-        "max": 23,
-        "state-avg": 7.7
-      },
-      "RETAIL": {
-        "text": "Retail",
-        "county-avg": 0,
-        "min": 7,
-        "max": 19,
-        "state-avg": 11.7
-      },
-      "MANUFACTURING": {
-        "text": "Manufacturing",
-        "county-avg": 0,
-        "min": 6,
-        "max": 33,
-        "state-avg": 14.8
-      },
-      "CONSTRUCTION": {
-        "text": "Construction",
-        "county-avg": 0,
-        "min": 3,
-        "max": 12,
-        "state-avg": 6.0
-      },
-      "AG": {
-        "text": "Ag",
-        "county-avg": 0,
-        "min": 0,
-        "max": 17,
-        "state-avg": 4.0
-      }
-    }
-  },
-  "EDU": {
-    "text": "Education",
-    "text-before": "",
-    "text-after": "%",
-    "categories": {
-      "subcategories": true,
-      "GRADUATE": {
-        "text": "Graduate",
-        "county-avg": 0,
-        "min": 2,
-        "max": 24,
-        "state-avg": 8.0
-      },
-      "BACHELORS": {
-        "text": "Bachelors",
-        "county-avg": 0,
-        "min": 7,
-        "max": 33,
-        "state-avg": 17.7
-      },
-      "SOME_COLLEGE": {
-        "text": "Some college",
-        "county-avg": 0,
-        "min": 25,
-        "max": 42,
-        "state-avg": 32.3
-      },
-      "HS_GRAD": {
-        "text": "Hs grad",
-        "county-avg": 0,
-        "min": 17,
-        "max": 47,
-        "state-avg": 32.9
-      },
-      "LESS_THAN_HS": {
-        "text": "Less than hs",
-        "county-avg": 0,
-        "min": 4,
-        "max": 22,
-        "state-avg": 9.0
-      }
-    }
-  },
-  "RACE": {
-    "text": "Race",
-    "text-before": "",
-    "text-after": "%",
-    "categories": {
-      "subcategories": true,
-      "TWO": {
-        "text": "Two",
-        "county-avg": 0,
-        "min": 0,
-        "max": 4,
-        "state-avg": 1.9
-      },
-      "OTHER": {
-        "text": "Other",
-        "county-avg": 0,
-        "min": 0,
-        "max": 8,
-        "state-avg": 1.4
-      },
-      "ASIAN": {
-        "text": "Asian",
-        "county-avg": 0,
-        "min": 0,
-        "max": 8,
-        "state-avg": 1.8
-      },
-      "INDIAN": {
-        "text": "Indian",
-        "county-avg": 0,
-        "min": 0,
-        "max": 8,
-        "state-avg": 0.3
-      },
-      "BLACK": {
-        "text": "Black",
-        "county-avg": 0,
-        "min": 0,
-        "max": 10,
-        "state-avg": 3.0
-      },
-      "WHITE": {
-        "text": "White",
-        "county-avg": 0,
-        "min": 80,
-        "max": 100,
-        "state-avg": 91.5
-      }
-    }
-  },
-  "POVERTY": {
-    "text": "Poverty",
-    "text-before": "",
-    "text-after": "%",
+    "text-after": "",
     "categories": {
       "subcategories": false,
-      "min": 6,
-      "max": 22,
+      "min": 3973,
+      "max": 438307,
+      "county-avg": 30934,
+      "state-avg": 3062553
+    }
+  },
+  "MEDIAN_AGE": {
+    "text": "Median age",
+    "text-before": "",
+    "text-after": "",
+    "categories": {
+      "subcategories": false,
+      "min": 26,
+      "max": 49,
       "county-avg": 0,
-      "state-avg": 12.4
+      "state-avg": 38.1
     }
   },
   "MEDIAN_HOUSEHOLD_INCOME": {
@@ -200,28 +62,166 @@ var slider_categories = {
       "state-avg": 51843
     }
   },
-  "MEDIAN_AGE": {
-    "text": "Median age",
+  "POVERTY": {
+    "text": "Poverty",
     "text-before": "",
     "text-after": "%",
     "categories": {
       "subcategories": false,
-      "min": 26,
-      "max": 49,
+      "min": 6,
+      "max": 22,
       "county-avg": 0,
-      "state-avg": 38.1
+      "state-avg": 12.4
     }
   },
-  "POPULATION": {
-    "text": "Population",
+  "RACE": {
+    "text": "Race",
     "text-before": "",
-    "text-after": "",
+    "text-after": "%",
     "categories": {
-      "subcategories": false,
-      "min": 3973,
-      "max": 438307,
-      "county-avg": 30934,
-      "state-avg": 3062553
+      "subcategories": true,
+      "WHITE": {
+        "text": "White",
+        "county-avg": 0,
+        "min": 80,
+        "max": 100,
+        "state-avg": 91.5
+      },
+      "BLACK": {
+        "text": "Black",
+        "county-avg": 0,
+        "min": 0,
+        "max": 10,
+        "state-avg": 3.0
+      },
+      "INDIAN": {
+        "text": "Indian",
+        "county-avg": 0,
+        "min": 0,
+        "max": 8,
+        "state-avg": 0.3
+      },
+      "ASIAN": {
+        "text": "Asian",
+        "county-avg": 0,
+        "min": 0,
+        "max": 8,
+        "state-avg": 1.8
+      },
+      "OTHER": {
+        "text": "Other",
+        "county-avg": 0,
+        "min": 0,
+        "max": 8,
+        "state-avg": 1.4
+      },
+      "TWO": {
+        "text": "Two",
+        "county-avg": 0,
+        "min": 0,
+        "max": 4,
+        "state-avg": 1.9
+      }
+    }
+  },
+  "EDU": {
+    "text": "Education",
+    "text-before": "",
+    "text-after": "%",
+    "categories": {
+      "subcategories": true,
+      "LESS_THAN_HS": {
+        "text": "Less than high school",
+        "county-avg": 0,
+        "min": 4,
+        "max": 22,
+        "state-avg": 9.0
+      },
+      "HS_GRAD": {
+        "text": "High school grad",
+        "county-avg": 0,
+        "min": 17,
+        "max": 47,
+        "state-avg": 32.9
+      },
+      "SOME_COLLEGE": {
+        "text": "Some college",
+        "county-avg": 0,
+        "min": 25,
+        "max": 42,
+        "state-avg": 32.3
+      },
+      "BACHELORS": {
+        "text": "Bachelor's degree",
+        "county-avg": 0,
+        "min": 7,
+        "max": 33,
+        "state-avg": 17.7
+      },
+      "GRADUATE": {
+        "text": "Graduate degree",
+        "county-avg": 0,
+        "min": 2,
+        "max": 24,
+        "state-avg": 8.0
+      }
+    }
+  },
+  "INDUSTRY": {
+    "text": "Industry",
+    "text-before": "",
+    "text-after": "%",
+    "categories": {
+      "subcategories": true,
+      "AG": {
+        "text": "Agriculture",
+        "county-avg": 0,
+        "min": 0,
+        "max": 17,
+        "state-avg": 4.0
+      },
+      "CONSTRUCTION": {
+        "text": "Construction",
+        "county-avg": 0,
+        "min": 3,
+        "max": 12,
+        "state-avg": 6.0
+      },
+      "MANUFACTURING": {
+        "text": "Manufacturing",
+        "county-avg": 0,
+        "min": 6,
+        "max": 33,
+        "state-avg": 14.8
+      },
+      "RETAIL": {
+        "text": "Retail",
+        "county-avg": 0,
+        "min": 7,
+        "max": 19,
+        "state-avg": 11.7
+      },
+      "FINANCE": {
+        "text": "Finance",
+        "county-avg": 0,
+        "min": 2,
+        "max": 23,
+        "state-avg": 7.7
+      },
+      "EDUCATION": {
+        "text": "Education",
+        "county-avg": 0,
+        "min": 17,
+        "max": 41,
+        "state-avg": 24.0
+      },
+      "ARTS": {
+        "text": "Arts",
+        "county-avg": 0,
+        "min": 1,
+        "max": 12,
+        "state-avg": 7.5
+      }
     }
   },
   "UNEMPLOYMENT": {
