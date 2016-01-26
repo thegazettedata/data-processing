7ed28993b660867015b2543ae7185a0834de9f8d
Chris Essig <csessig86@gmail.com>
2016-01-25 18:06:15 -0600

    Download raw data

--- projects/caucus-results/process.sh
+++ projects/caucus-results/process.sh
@@ -8,9 +8,9 @@ function downloadData() {
 		echo "- Loop for $party party"
 		
 		# County data
-		curl -X GET --header "Accept: application/json" "https://www."$party"caucuses.com/api/CountyCandidateResults" > raw/$party-counties.json
+		curl -X GET --header "Accept: application/json" "https://www."$party"caucuses.com/api/CountyCandidateResults" > raw_feeds/$party-counties.json
 		# Statewide data
-		curl -X GET --header "Accept: application/json" "https://www."$party"caucuses.com/api/StateCandidateResults" > raw/$party-statewide.json
+		curl -X GET --header "Accept: application/json" "https://www."$party"caucuses.com/api/StateCandidateResults" > raw_feeds/$party-statewide.json
 
 	done
 }
